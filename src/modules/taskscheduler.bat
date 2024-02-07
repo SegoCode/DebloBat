2dc6393c7c0080d2d295c423c4f94910e0d1c3e3
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-02-07 21:14:33 +0100

    Update taskscheduler.bat

--- src/modules/taskscheduler.bat
+++ src/modules/taskscheduler.bat
@@ -15,64 +15,70 @@ echo.
 
 schtasks /query /tn "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
 if not %errorlevel% == 0 (
-	echo    [1]  Consolidator                 = [[1;32m Disabled [m]
+	echo    [1]  Consolidator                       = [[1;32m Disabled [m]
 ) else (
-	echo    [1]  Consolidator                 = [[1;31m Enabled [m]
+	echo    [1]  Consolidator                       = [[1;31m Enabled [m]
 )
 
 schtasks /query /tn "\Microsoft\Windows\PI\Sqm-Tasks" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
-if not %errorlevel% == 0 (
-	echo    [2]  Sqm-Tasks                    = [[1;32m Disabled [m]
-) else (
-	echo    [2]  Sqm-Tasks                    = [[1;31m Enabled [m]
+if not %errorlevel% == 0 ( 
+	echo    [2]  Sqm-Tasks                           = [[1;32m Disabled [m]
+) else ( 
+	echo    [2]  Sqm-Tasks                           = [[1;31m Enabled [m]
 )
 
 
 schtasks /query /tn "\Microsoft\Windows\Autochk\Proxy" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
 if not %errorlevel% == 0 (
-	echo    [3]  Proxy                        = [[1;32m Disabled [m]
+	echo    [3]  Proxy                               = [[1;32m Disabled [m]
 ) else (
-	echo    [3]  Proxy                        = [[1;31m Enabled [m]
+	echo    [3]  Proxy                               = [[1;31m Enabled [m]
 )
-
 
 schtasks /query /tn "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
 if not %errorlevel% == 0 (
-	echo    [4]  DiskDiagnosticDataCollector  = [[1;32m Disabled [m]
+	echo    [4]  DiskDiagnosticDataCollector         = [[1;32m Disabled [m]
 ) else (
-	echo    [4]  DiskDiagnosticDataCollector  = [[1;31m Enabled [m]
+	echo    [4]  DiskDiagnosticDataCollector         = [[1;31m Enabled [m]
 )
 
 schtasks /query /tn "\Microsoft\Windows\NetTrace\GatherNetworkInfo" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
 if not %errorlevel% == 0 (
-	echo    [5]  GatherNetworkInfo            = [[1;32m Disabled [m]
+	echo    [5]  GatherNetworkInfo                   = [[1;32m Disabled [m]
 ) else (
-	echo    [5]  GatherNetworkInfo            = [[1;31m Enabled [m]
+	echo    [5]  GatherNetworkInfo                   = [[1;31m Enabled [m]
 )
 
 schtasks /query /tn "\Microsoft\Windows\Device Information\Device" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
 if not %errorlevel% == 0 (
-	echo    [6]  Device Census                = [[1;32m Disabled [m]
+	echo    [6]  Device Census                       = [[1;32m Disabled [m]
 ) else (
-	echo    [6]  Device Census                = [[1;31m Enabled [m]
+	echo    [6]  Device Census                       = [[1;31m Enabled [m]
 )
 
 
 schtasks /query /tn "\Microsoft\Windows\Windows Error Reporting\QueueReporting" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
 if not %errorlevel% == 0 (
-	echo    [7]  Windows Error Reporting      = [[1;32m Disabled [m]
+	echo    [7]  Windows Error Reporting              = [[1;32m Disabled [m]
 ) else (
-	echo    [7]  Windows Error Reporting      = [[1;31m Enabled [m]
+	echo    [7]  Windows Error Reporting              = [[1;31m Enabled [m]
 )
 
 schtasks /query /tn "\Microsoft\Windows\Feedback\Siuf\DmClient" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
 if not %errorlevel% == 0 (
-	echo    [8]  DmClient                     = [[1;32m Disabled [m]
+	echo    [8]  DmClient                           = [[1;32m Disabled [m]
 ) else (
-	echo    [8]  DmClient                     = [[1;31m Enabled [m]
+	echo    [8]  DmClient                           = [[1;31m Enabled [m]
 )
 
-echo    [9]  Apply all                    = [[1;31m * [m]
+schtasks /query /tn "\MicrosoftEdgeUpdateTaskMachineUA" | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
+if not %errorlevel% == 0 (
+	echo    [9]  MicrosoftEdgeUpdateTaskMachineUA   = [[1;32m Disabled [m]
+) else (
+	echo    [9]  MicrosoftEdgeUpdateTaskMachineUA   = [[1;31m Enabled [m]
+)
+
+echo    [10]  Apply all                    = [[1;31m * [m]
 echo.
 echo    [0]  Return to menu
 
@@ -88,6 +94,7 @@ if %N%==5 (set task="\Microsoft\Windows\NetTrace\GatherNetworkInfo")
 if %N%==6 (set task="\Microsoft\Windows\Device Information\Device")
 if %N%==7 (set task="\Microsoft\Windows\Windows Error Reporting\QueueReporting")
 if %N%==8 (set task="\Microsoft\Windows\Feedback\Siuf\DmClient")
+if %N%==9 (set task="\MicrosoftEdgeUpdateTaskMachineUA")
 
 set tasks="\Microsoft\Windows\Feedback\Siuf\DmClient" ^
            "\Microsoft\Windows\Windows Error Reporting\QueueReporting" ^
@@ -96,10 +103,11 @@ set tasks="\Microsoft\Windows\Feedback\Siuf\DmClient" ^
 		   "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" ^
            "\Microsoft\Windows\Autochk\Proxy" ^
 		   "\Microsoft\Windows\PI\Sqm-Tasks" ^
+		   "\MicrosoftEdgeUpdateTaskMachineUA" ^
            "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
 
 set /A count=1
-if %N%==9 (
+if %N%==10 (
     for %%t in (%tasks%) do (
         schtasks /query /tn %%t | findstr /r "Listo | Ready | Running | Queued" > nul 2>&1
         if %errorlevel% equ 0 (
