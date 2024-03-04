d6f07855e0ae401d1b44687b4dca08d709c4df0b
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-03-04 16:23:10 +0100

    Update otherlocalgroup.bat

--- src/modules/otherlocalgroup.bat
+++ src/modules/otherlocalgroup.bat
@@ -57,9 +57,9 @@ if not %errorlevel% == 1 (
 
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith > nul 2>&1
 if not %errorlevel% == 1 (
-	echo    [7]  Open with the store                          = [[1;32m Disabled [m]
+	echo    [7]  Option "Look For An App In The Store"        = [[1;32m Disabled [m]
 ) else (
-	echo    [7]  Open with the store                          = [[1;31m Enabled [m]
+	echo    [7]  Option "Look For An App In The Store"        = [[1;31m Enabled [m]
 )
 
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-time Protection" /v DisableRealtimeMonitoring > nul 2>&1
@@ -134,57 +134,64 @@ if not %errorlevel% == 1 (
     echo    [17] Windows Defender SmartScreen                 = [[1;31m Enabled [m]
 )
 
-reg query "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\QuietHours" /v Enable > nul 2>&1
+reg query "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v DisableNotificationCenter > nul 2>&1
 if not %errorlevel% == 1 (
-    echo    [18] Windows Notifications                        = [[1;31m Enabled [m]
-) else (
     echo    [18] Windows Notifications                        = [[1;32m Disabled [m]
+) else (
+    echo    [18] Windows Notifications                        = [[1;31m Enabled [m]
 )
 
-reg query "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v DisableSearchBoxSuggestions > nul 2>&1
+reg query "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v VerboseStatus > nul 2>&1
 if not %errorlevel% == 1 (
-	echo    [19] Search box suggestions in Explorer           = [[1;32m Disabled [m]
+    echo    [19] Simple Verbose in loader                     = [[1;32m Disabled [m]
 ) else (
-	echo    [19] Search box suggestions in Explorer           = [[1;31m Enabled [m]
+    echo    [19] Simple Verbose in loader                     = [[1;31m Enabled [m]
 )
 
 reg query "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightFeatures > nul 2>&1
 if not %errorlevel% == 1 (
-    echo    [20] Disable Windows Spotlight Features           = [[1;32m Disabled [m]
+    echo    [20] Windows Spotlight Features                   = [[1;32m Disabled [m]
 ) else (
-    echo    [20] Disable Windows Spotlight Features           = [[1;31m Enabled [m]
+    echo    [20] Windows Spotlight Features                   = [[1;31m Enabled [m]
 )
 
 reg query "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v MSAOptional > nul 2>&1
 if %errorlevel% == 1 (
-    echo    [21] Microsoft accounts required                  = [[1;32m Disabled [m]
+    echo    [21] Microsoft accounts required in store         = [[1;31m Enabled [m]
 ) else (
-    echo    [21] Microsoft accounts required                  = [[1;31m Enabled [m]
+    echo    [21] Microsoft accounts required in store         = [[1;32m Disabled [m]
 )
 
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" /v EnableFirewall > nul 2>&1
 if not %errorlevel% == 1 (
-    echo    [22] Windows Firewall                             = [[1;31m Enabled [m]
+    echo    [22] Windows Firewall                             = [[1;32m Disabled [m]
 ) else (
-    echo    [22] Windows Firewall                             = [[1;32m Disabled [m] 
+    echo    [22] Windows Firewall                             = [[1;31m Enabled [m] 
 )
 
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v DisableLogonBackgroundImage > nul 2>&1
 if not %errorlevel% == 1 (
-    echo    [23] Logon Background Image Disabled              = [[1;32m Disabled [m]
+    echo    [23] Logon Background Image                       = [[1;32m Disabled [m]
 ) else (
-    echo    [23] Logon Background Image Disabled              = [[1;31m Enabled [m]
+    echo    [23] Logon Background Image                       = [[1;31m Enabled [m]
 )
 
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" /v EnableFeeds > nul 2>&1
 if not %errorlevel% == 1 (
-    echo    [24] Windows Feeds Enabled                        = [[1;31m Enabled [m]
+    echo    [24] Windows Feeds                                = [[1;32m Disabled [m]
 ) else (
-    echo    [24] Windows Feeds Enabled                        = [[1;32m Disabled [m]
+    echo    [24] Windows Feeds                                = [[1;31m Enabled [m]
+)
+
+reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAMeetNow > nul 2>&1
+if not %errorlevel% == 1 (
+    echo    [25] Meet Now icon                                = [[1;32m Disabled [m]
+) else (
+    echo    [25] Meet Now icon                                = [[1;31m Enabled [m]
 )
 
 
-echo    [25] Apply all                                    = [[1;31m * [m]
+echo    [26] Apply all                                    = [[1;31m * [m]
 echo.
 echo    [0]  Exit
 
@@ -206,19 +213,19 @@ if %N%==11 (set path="HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Gam
 if %N%==12 (set path="HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Messaging" && set key=AllowMessageSync && set value=0)
 if %N%==13 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" && set key=PublishUserActivities && set value=0)
 if %N%==14 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" && set key=AllowCloudSearch && set value=0)
-if %N%==15 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" && set key=ConnectedSearchUseWeb && set value=0)
+if %N%==15 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" && set key=ConnectedSearchUseWeb && set value=1)
 if %N%==16 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" && set key=DODownloadMode && set value=0)
 if %N%==17 (set path="HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\System" && set key=EnableSmartScreen && set value=0)
-if %N%==18 (set path="HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\QuietHours" && set key=Enable && set value=1)
-if %N%==19 (set path="HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer" && set key=DisableSearchBoxSuggestions && set value=1)
+if %N%==18 (set path="HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" && set key=DisableNotificationCenter && set value=1)
+if %N%==19 (set path="HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" && set key=VerboseStatus && set value=1)
 if %N%==20 (set path="HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" && set key=DisableWindowsSpotlightFeatures && set value=1)
-if %N%==21 (set path="HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" && set key=MSAOptional && set value=1)
+if %N%==21 (set path="HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" && set key=MSAOptional && set value=1) 
 if %N%==22 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" && set key=EnableFirewall && set value=0)
 if %N%==23 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" && set key=DisableLogonBackgroundImage && set value=1)
 if %N%==24 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" && set key=EnableFeeds && set value=0)
+if %N%==25 (set path="HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" && set key=HideSCAMeetNow && set value=1)
 
-
-if %N%==25 (set loopcount=24 && goto APPLYALLOTHERLOCALGROUP)
+if %N%==26 (set loopcount=25 && goto APPLYALLOTHERLOCALGROUP)
 if %N%==0 (goto INIT)
 
 reg query %path% /v %key% > nul 2>&1
@@ -245,17 +252,17 @@ if %loopcount%==11 (set path="HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Win
 if %loopcount%==12 (set path="HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\Messaging" && set key=AllowMessageSync && set value=0)
 if %loopcount%==13 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" && set key=PublishUserActivities && set value=0)
 if %loopcount%==14 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" && set key=AllowCloudSearch && set value=0)
-if %loopcount%==15 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" && set key=ConnectedSearchUseWeb && set value=0)
+if %loopcount%==15 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" && set key=ConnectedSearchUseWeb && set value=1)
 if %loopcount%==16 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" && set key=DODownloadMode && set value=0)
 if %loopcount%==17 (set path="HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\System" && set key=EnableSmartScreen && set value=0)
-if %loopcount%==18 (set path="HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\QuietHours" && set key=Enable && set value=1)
-if %loopcount%==19 (set path="HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer" && set key=DisableSearchBoxSuggestions && set value=1)
+if %loopcount%==18 (set path="HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" && set key=DisableNotificationCenter && set value=1)
+if %loopcount%==19 (set path="HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" && set key=VerboseStatus && set value=1)
 if %loopcount%==20 (set path="HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" && set key=DisableWindowsSpotlightFeatures && set value=1)
-if %loopcount%==21 (set path="HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" && set key=MSAOptional && set value=1)
+if %loopcount%==21 (set path="HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" && set key=MSAOptional && set value=1) 
 if %loopcount%==22 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" && set key=EnableFirewall && set value=0)
 if %loopcount%==23 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" && set key=DisableLogonBackgroundImage && set value=1)
 if %loopcount%==24 (set path="HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" && set key=EnableFeeds && set value=0)
-
+if %loopcount%==25 (set path="HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" && set key=HideSCAMeetNow && set value=1)
 
 reg query %path% /v %key% > nul 2>&1
 if not %errorlevel% == 1 (
