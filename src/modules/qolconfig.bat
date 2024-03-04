3c068c3bc4a60e7e930e65517c9749477e27305e
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-03-04 16:23:07 +0100

    Update qolconfig.bat

--- src/modules/qolconfig.bat
+++ src/modules/qolconfig.bat
@@ -13,16 +13,6 @@ echo    Quality of life configurations
 echo    -----------------------------------------------
 echo.
 
-
-::TODO: Implement
-:: echo --- Disable Firewall through registry
-:: Policy based
-:: reg add "HKLM\SOFTWARE\Policies\Microsoft\WindowsFirewall\StandardProfile" /v "EnableFirewall" /t REG_DWORD /d 0 /f
-:: reg add "HKLM\SOFTWARE\Policies\Microsoft\WindowsFirewall\PublicProfile" /v "EnableFirewall" /t REG_DWORD /d 0 /f
-:: reg add "HKLM\SOFTWARE\Policies\Microsoft\WindowsFirewall\PrivateProfile" /v "EnableFirewall" /t REG_DWORD /d 0 /f
-:: reg add "HKLM\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile" /v "EnableFirewall" /t REG_DWORD /d 0 /f
-
-
 reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt > nul 2>&1 && (
 	reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt | find "0x0" > nul 2>&1
 )
@@ -53,33 +43,32 @@ if not %errorlevel% == 1 (
 )
 
 
-reg query "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith > nul 2>&1 && (
-	reg query "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith  | find "0x1" > nul 2>&1
+reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton > nul 2>&1 && (
+	reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton | find "0x0" > nul 2>&1
 )
 if not %errorlevel% == 1 (
-	echo    [4]  Turn off "Look For An App In The Store"   = [[1;32m Enabled [m]
+	echo    [4]  Hide task timeline button                 = [[1;32m Enabled [m]
 ) else (
-	echo    [4]  Turn off "Look For An App In The Store"   = [[1;31m Disabled [m]
+	echo    [4]  Hide task timeline button                 = [[1;31m Disabled [m]
 )
 
 
 reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v DefaultTTL > nul 2>&1 && (
 	reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v DefaultTTL  | find "0x40" > nul 2>&1
 )
-
 if not %errorlevel% == 1 (
-	echo    [5]  Change TTL to 64                          = [[1;32m Enabled [m]
+	echo    [5]  Change TTL to 64 for security reasons     = [[1;32m Enabled [m]
 ) else (
-	echo    [5]  Change TTL to 64                          = [[1;31m Disabled [m]
+	echo    [5]  Change TTL to 64 for security reasons     = [[1;31m Disabled [m]
 )
 
 reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" /v PeopleBand > nul 2>&1 && (
 	reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" /v PeopleBand | find "0x0" > nul 2>&1
 )
 if not %errorlevel% == 1 (
-	echo    [6]  Remove the people from the taskbar        = [[1;32m Enabled [m]
+	echo    [6]  Hide the people from the taskbar          = [[1;32m Enabled [m]
 ) else (
-	echo    [6]  Remove the people from the taskbar        = [[1;31m Disabled [m]
+	echo    [6]  Hide the people from the taskbar          = [[1;31m Disabled [m]
 )
 
 reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled > nul 2>&1 && (
@@ -136,60 +125,32 @@ if not %errorlevel% == 1 (
     echo    [12] Quick-Access in Explorer                  = [[1;31m Disabled [m]
 )
 
-reg query "HKCU\Control Panel\Desktop" /v JPEGImportQuality > nul 2>&1 && (
-    reg query "HKCU\Control Panel\Desktop" /v JPEGImportQuality | find "0x0" > nul 2>&1
-)
-if not %errorlevel% == 1 (
-    echo    [13] Desktop wallpaper quality reduction       = [[1;31m Disabled [m]
-) else (
-    echo    [13] Desktop wallpaper quality reduction       = [[1;32m Enabled [m]
-)
-
-
 reg query "HKCU\Control Panel\Desktop" /v Wallpaper > nul 2>&1 && (
-    reg query "HKCU\Control Panel\Desktop" /v Wallpaper | find "wallpaper_deblobat.png" > nul 2>&1
+    reg query "HKCU\Control Panel\Desktop" /v Wallpaper | find "Deblobat" > nul 2>&1
 )
 if not %errorlevel% == 1 (
-    echo    [14] Custom Deblobat wallpaper                 = [[1;32m Enabled [m]
+    echo    [13] Deblobat wallpaper and theme              = [[1;32m Enabled [m]
 ) else (
-    echo    [14] Custom Deblobat wallpaper                 = [[1;31m Disabled [m]
+    echo    [13] Deblobat wallpaper and theme              = [[1;31m Disabled [m]
 )
 
-reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAMeetNow > nul 2>&1 && (
-	reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAMeetNow | find "0x0" > nul 2>&1
-)
-if not %errorlevel% == 1 (
-	echo    [15] Hide Meet Now icon in Taskbar             = [[1;32m Enabled [m]
-) else (
-	echo    [15] Hide Meet Now icon in Taskbar             = [[1;31m Disabled [m]
-)
-
-reg query "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v HidePeopleBar > nul 2>&1 && (
-	reg query "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v HidePeopleBar | find "0x0" > nul 2>&1
-)
-if not %errorlevel% == 1 (
-	echo    [16] Hide People Bar in taskbar                = [[1;32m Enabled [m]
-) else (
-	echo    [16] Hide People Bar in taskbar                = [[1;31m Disabled [m]
-)
 
 reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode > nul 2>&1 && (
     reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode | find "0x0" > nul 2>&1
 )
 if not %errorlevel% == 1 (
-    echo    [17] Hide Searchbox Taskbar                    = [[1;32m Enabled [m]
+    echo    [14] Hide Searchbox Taskbar                    = [[1;32m Enabled [m]
 ) else (
-    echo    [17] Hide Searchbox Taskbar                    = [[1;31m Disabled [m]
+    echo    [14] Hide Searchbox Taskbar                    = [[1;31m Disabled [m]
 )
-
 
 reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarGlomLevel > nul 2>&1 && (
     reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarGlomLevel | find "0x2" > nul 2>&1
 )
 if not %errorlevel% == 1 (
-    echo    [18] No grouping in taskbar                    = [[1;32m Enabled [m]
+    echo    [15] No grouping in taskbar                    = [[1;32m Enabled [m]
 ) else (
-    echo    [18] No grouping in taskbar                    = [[1;31m Disabled [m]
+    echo    [15] No grouping in taskbar                    = [[1;31m Disabled [m]
 )
 
 
@@ -197,16 +158,16 @@ reg query "HKEY_CURRENT_USER\Control Panel\Mouse" /v MouseThreshold1 > nul 2>&1 
 	reg query "HKEY_CURRENT_USER\Control Panel\Mouse" /v MouseThreshold1 | find "0" > nul 2>&1
 )
 if not %errorlevel% == 1 (
-	echo    [19] Disable Mouse Acceleration                = [[1;32m Enabled [m]
+	echo    [16] Disable Mouse Acceleration                = [[1;32m Enabled [m]
 ) else (
-	echo    [19] Disable Mouse Acceleration                = [[1;31m Disabled [m]
+	echo    [16] Disable Mouse Acceleration                = [[1;31m Disabled [m]
 )
 
 
 if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe_bak" (
-	echo    [20] Disable Edge                              = [[1;32m Enabled [m]
+	echo    [17] Disable Edge                              = [[1;32m Enabled [m]
 ) else (
-	echo    [20] Disable Edge                              = [[1;31m Disabled [m]
+	echo    [17] Disable Edge                              = [[1;31m Disabled [m]
 )
 
 
@@ -232,9 +193,10 @@ if %N%==1 (
 
 if %N%==2 (
     reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons > nul 2>&1
-    if %ERRORLEVEL% NEQ 0 (
-        reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f > nul 2>&1
+    if !ERRORLEVEL! NEQ 0 (
+        reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 0 /f > nul 2>&1
     )
+
     reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons | find "0x1" > nul 2>&1
     if not !ERRORLEVEL! == 1 (
         reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 0 /f > nul 2>&1
@@ -256,17 +218,19 @@ if %N%==3 (
 
 
 if %N%==4 (
-    reg query "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith > nul 2>&1
-    if %ERRORLEVEL% NEQ 0 (
-        reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith /t REG_DWORD /d 1 /f > nul 2>&1
+    reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton > nul 2>&1
+    if !ERRORLEVEL! NEQ 0 (
+        reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 1 /f > nul 2>&1
     )
-	reg query "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith  | find "0x1" > nul 2>&1
-	if not !ERRORLEVEL! == 1 (
-		reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith /t REG_DWORD /d 0 /f > nul 2>&1
-	) else (
-		reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v NoUseStoreOpenWith /t REG_DWORD /d 1 /f > nul 2>&1
-	)
+
+    reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton | find "0x0" > nul 2>&1
+    if not !ERRORLEVEL! == 1 (
+        reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 1 /f > nul 2>&1
+    ) else (
+        reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 0 /f > nul 2>&1
+    )
 )
+
 
 if %N%==5 (
     reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v DefaultTTL  > nul 2>&1
@@ -350,56 +314,21 @@ if %N%==12 (
 )
 
 if %N%==13 (
-   reg query "HKCU\Control Panel\Desktop" /v JPEGImportQuality > nul 2>&1
-    if %ERRORLEVEL% NEQ 0 (
-        reg add "HKCU\Control Panel\Desktop" /v JPEGImportQuality /t REG_DWORD /d 0 /f > nul 2>&1
-    )
-    reg query "HKCU\Control Panel\Desktop" /v JPEGImportQuality | find "0x64" > nul 2>&1
-    if not !ERRORLEVEL! == 1 (
-        reg add "HKCU\Control Panel\Desktop" /v JPEGImportQuality /t REG_DWORD /d 0 /f > nul 2>&1
-    ) else (
-        reg add "HKCU\Control Panel\Desktop" /v JPEGImportQuality /t REG_DWORD /d 100 /f > nul 2>&1
-    )
-)
-if %N%==14 (
-    reg query "HKCU\Control Panel\Desktop" /v Wallpaper | find "wallpaper_deblobat.png" > nul 2>&1
+    reg query "HKCU\Control Panel\Desktop" /v Wallpaper | find "Deblobat" > nul 2>&1
     if not !ERRORLEVEL! == 1 (
         reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f > nul 2>&1
+        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 0 /f > nul 2>&1
+        reg add "HKCU\Software\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 0 /f > nul 2>&1
+        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v EnableTransparency /t REG_DWORD /d 1 /f > nul 2>&1
     ) else (
-    :: Create DebloBat directory in %APPDATA% if it doesn't exist
-    IF NOT EXIST "%APPDATA%\DebloBat" mkdir "%APPDATA%\DebloBat"
-
-    :: Download image using Powershell and save it in the DebloBat directory
-    PowerShell -Command "$imageUrl = 'https://raw.githubusercontent.com/SegoCode/DebloBat/main/media/wallpaper.png'; $DebloBatPath = [System.IO.Path]::Combine($env:APPDATA, 'DebloBat', 'wallpaper_deblobat.png'); Invoke-WebRequest -Uri $imageUrl -OutFile $DebloBatPath;"
-
-    :: Change registry key using Reg command
-    reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %APPDATA%\DebloBat\wallpaper_deblobat.png /f
-
-    :: Apply the wallpaper change using Powershell
-    PowerShell -Command "Add-Type -TypeDefinition 'namespace Wallpaper{using System;using System.Runtime.InteropServices;public class Setter{[DllImport(\"user32.dll\", CharSet = CharSet.Auto)] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);}}';$SPI_SETDESKWALLPAPER = 0x0014;$SPIF_UPDATEINIFILE = 0x01;$SPIF_SENDCHANGE = 0x02;[Wallpaper.Setter]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, '%APPDATA%\DebloBat\wallpaper_deblobat.png', $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE);"
+        "%~dp0\sources\Deblobat.deskthemepack" & timeout /t 03 /nobreak > nul 2>&1 & taskkill /f /im systemsettings.exe > nul 2>&1
+        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f > nul 2>&1
+        reg add "HKCU\Software\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f > nul 2>&1
+        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v EnableTransparency /t REG_DWORD /d 0 /f > nul 2>&1
     )
 )
 
-
-if %N%==15 (
-	reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAMeetNow | find "0x0" > nul 2>&1
-	if not !ERRORLEVEL! == 1 (
-		reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAMeetNow /t REG_DWORD /d 1 /f > nul 2>&1
-	) else (
-		reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v HideSCAMeetNow /t REG_DWORD /d 0 /f > nul 2>&1
-	)
-)
-
-if %N%==16 (
-	reg query "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v HidePeopleBar | find "0x0" > nul 2>&1
-	if not !ERRORLEVEL! == 1 (
-		reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v HidePeopleBar /t REG_DWORD /d 1 /f > nul 2>&1
-	) else (
-		reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v HidePeopleBar /t REG_DWORD /d 0 /f > nul 2>&1
-	)
-)
-
-if %N%==17 (
+if %N%==14 (
     reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode | find "0x0" > nul 2>&1
     if not !ERRORLEVEL! == 1 (
         reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f > nul 2>&1
@@ -408,7 +337,7 @@ if %N%==17 (
     )
 )
 
-if %N%==18 (
+if %N%==15 (
     reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarGlomLevel | find "0x2" > nul 2>&1
     if not !ERRORLEVEL! == 1 (
         reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarGlomLevel /t REG_DWORD /d 0 /f > nul 2>&1
@@ -418,7 +347,7 @@ if %N%==18 (
 )
 
 
-if %N%==19 (
+if %N%==16 (
 	reg query "HKEY_CURRENT_USER\Control Panel\Mouse" /v MouseThreshold1 | find "0"
 	if not !ERRORLEVEL! == 1 (
 		reg add "HKEY_CURRENT_USER\Control Panel\Mouse" /v MouseThreshold1 /t REG_SZ /d 6 /f > nul 2>&1
@@ -432,7 +361,7 @@ if %N%==19 (
 	)
 )
 
-if %N%==20 (
+if %N%==17 (
 	if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe_bak" (
 		rename "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe_bak" "msedge.exe" > nul 2>&1
 	) else (
