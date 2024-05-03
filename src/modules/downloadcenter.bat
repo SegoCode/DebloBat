49c2fc21e664457c94f817687462f13a17e14a2b
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-05-04 00:05:28 +0200

    Update downloadcenter.bat

--- src/modules/downloadcenter.bat
+++ src/modules/downloadcenter.bat
@@ -8,184 +8,96 @@ MODE 88,35
 cls
 echo.
 echo    Deblo.bat -[1;36m Download center [m
-echo    Always latest version and oficial links
+echo    Always latest version and official links
 echo    -----------------------------------------------
 echo.
-
+echo    This tool utilizes Scoop repositories to fetch the latest 
+echo    versions of software. It won't install the software, 
+echo    instead, it creates an installation folder on your 
+echo    desktop where the downloads are saved. You can then 
+echo    decide how to proceed with the installers.
+echo.
 ping -n 2 8.8.8.8 > nul
 if not %errorlevel% == 1 (
-	echo    Internet Status            = [[1;32m Online [m]
+    echo    Internet Status              = [[1;32m Online [m]
 ) else (
-	echo    Internet Status            = [[1;31m Offline [m]
+    echo    Internet Status              = [[1;31m Offline [m]
 )
 echo.
-
 cd %~dp0
 
-if exist firefox-latest.exe (
-	echo    [1]  Mozilla Firefox       = [[1;32m Install [m]
-) else (
-	echo    [1]  Mozilla Firefox       = [[1;32m Download [m]
-)
-
-if exist vlc.exe (
-	echo    [2]  VLC                   = [[1;32m Install [m]
-) else (
-	echo    [2]  VLC                   = [[1;32m Download [m]
-)
-
-
-if exist SteamSetup.exe (
-	echo    [3]  Steam client          = [[1;32m Install [m]
-) else (
-	echo    [3]  Steam client          = [[1;32m Download [m]
-)
-
-if exist 7z-x64.exe (
-	echo    [4]  7Zip                  = [[1;32m Install [m]
-) else (
-	echo    [4]  7Zip                  = [[1;32m Download [m]
-)
-
-if exist transmission-win64-setup.exe (
-	echo    [5]  transmission          = [[1;32m Install [m]
-) else (
-	echo    [5]  transmission          = [[1;32m Download [m]
-)
-
-if exist DiscordSetup.exe (
-    echo    [6]  Discord               = [[1;32m Install [m]
-) else (
-    echo    [6]  Discord               = [[1;32m Download [m]
-)
-
-if exist FileConverter-setup.msi (
-    echo    [7]  FileConverter         = [[1;32m Install [m]
-) else (
-    echo    [7]  FileConverter         = [[1;32m Download [m]
-)
-
+echo    [1] Mozilla Firefox          = [[1;32m Download [m]
+echo    [2] VLC Media Player         = [[1;32m Download [m]
+echo    [3] 7-Zip                    = [[1;32m Download [m]
+echo    [4] qBittorrent Enhanced     = [[1;32m Download [m]
+echo    [5] Steam                    = [[1;32m Download [m]
+echo    [6] Discord                  = [[1;32m Download [m]
+echo    [7] File Converter           = [[1;32m Download [m]
+echo    [8] Telegram                 = [[1;32m Download [m]
+echo    [9] Spotify                  = [[1;32m Download [m]
+echo    [10] SimpleWall              = [[1;32m Download [m]
+echo    [11] Ungoogled Chromium      = [[1;32m Download [m]
+echo    [12] Brave Browser           = [[1;32m Download [m]
+echo    [13] qView                   = [[1;32m Download [m]
+echo    [14] Sublime Text            = [[1;32m Download [m]
+echo    [14] Flameshot               = [[1;32m Download [m]
 
 echo.
-echo    [0]  Return to menu
-
+echo    [0] Return to menu
 echo.
 
 set /P N=Select your task and press Enter ^>
 
-cls
-echo.
-echo    Deblo.bat -[1;36m Download center [m
-echo    Always latest version and oficial links
-echo    -----------------------------------------------
-echo.
-echo    Loading, Please wait...   
-
 setlocal
-if %N%==1 (
-    if exist firefox-latest.exe (
-        firefox-latest.exe /S > nul 2>&1
-        del firefox-latest.exe > nul 2>&1
-        goto INSTALLCOMPLETED
-    )
-
-    where curl > nul 2>&1
-    if %ERRORLEVEL% == 0 (
-        curl -L "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64" -o firefox-latest.exe > nul 2>&1
-    )
-)
-
-if %N%==2 (
-    if exist vlc.exe (
-        vlc.exe /S > nul 2>&1
-        del vlc.exe > nul 2>&1
-        goto INSTALLCOMPLETED
-    )
-
-    where curl > nul 2>&1
-    if %ERRORLEVEL% == 0 (
-        curl -L "https://ftp.free.org/mirrors/videolan/vlc/3.0.20/win32/vlc-3.0.20-win32.exe" -o vlc.exe > nul 2>&1
-    )
-)
-
-if %N%==3 (
-    if exist SteamSetup.exe (
-        SteamSetup.exe /S > nul 2>&1
-        del SteamSetup.exe > nul 2>&1
-        goto INSTALLCOMPLETED
-    )
-
-    where curl > nul 2>&1
-    if %ERRORLEVEL% == 0 (
-        curl -L "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -o SteamSetup.exe > nul 2>&1
-    )
-)
-
-if %N%==4 (
-    if exist 7z-x64.exe (
-        7z-x64.exe /S > nul 2>&1
-        del 7z-x64.exe > nul 2>&1
-        goto INSTALLCOMPLETED
-    )
-
-    where curl > nul 2>&1
-    if %ERRORLEVEL% == 0 (
-        curl -L "https://www.7-zip.org/a/7z2401-x64.exe" -o 7z-x64.exe > nul 2>&1
-    )
-)
-
-if %N%==5 (
-    if exist transmission-win64-setup.exe (
-        transmission-win64-setup.exe /s > nul 2>&1
-        del transmission-win64-setup.exe > nul 2>&1
-        goto INSTALLCOMPLETED
-    )
-
-    where curl > nul 2>&1
-    if %ERRORLEVEL% == 0 (
-        curl -L "https://github.com/transmission/transmission/releases/download/4.0.5/transmission-4.0.5-x64.msi" -o transmission-win64-setup.exe > nul 2>&1
-    )
-)
-
-if %N%==6 (
-    if exist DiscordSetup.exe (
-        DiscordSetup.exe -s > nul 2>&1
-        del DiscordSetup.exe > nul 2>&1
-        goto INSTALLCOMPLETED
-    )
-
-    where curl > nul 2>&1
-    if %ERRORLEVEL% == 0 (
-        curl -L "https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9011/DiscordSetup.exe" -o DiscordSetup.exe > nul 2>&1
-    )
-)
-
-
-if %N%==7 (
-    if exist FileConverter-setup.msi (
-        msiexec /i FileConverter-setup.msi /quiet /norestart
-        del FileConverter-setup.msi > nul 2>&1
-        goto INSTALLCOMPLETED
-    )
-    PowerShell -Command "Invoke-WebRequest -Uri ((((Invoke-WebRequest -UseBasicParsing -Uri 'https://api.github.com/repos/Tichau/FileConverter/releases/latest' | Select-Object).Content) | ConvertFrom-Json).assets[0].browser_download_url) -OutFile FileConverter-setup.msi" > nul 2>&1
-)
-
-endlocal
-if %N%==0 (goto INIT)
-goto DOWNLOADCENTER
-:INSTALLCOMPLETED
 cls
-echo.
-echo    Deblo.bat -[1;36m Download center [m
-echo    Always latest version and oficial links
-echo    -----------------------------------------------
-echo.
-echo    The software has been successfully installed
-echo    Press enter to return to the menu
-pause > nul 2>&1
+if %N%==1 (
+     powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "firefox"
+)
+if %N%==2 (
+     powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "vlc"
+)
+if %N%==3 (
+     powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "7zip"
+)
+if %N%==4 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "qbittorrent-enhanced"
+)
+if %N%==5 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "steam"
+)
+if %N%==6 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "discord"
+)
+if %N%==7 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "file-converter"
+)
+if %N%==8 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "telegram"
+)
+if %N%==9 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "spotify"
+)
+if %N%==10 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "simplewall"
+)
+if %N%==11 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "ungoogled-chromium"
+)
+if %N%==12 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "brave"
+)
+if %N%==13 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "qview"
+)
+if %N%==14 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "sublime-text"
+)
+if %N%==15 (
+    powershell -File ".\utils\chibiScoop.ps1" -SoftwareName "flameshot"
+)
+
+:: TODO: Continue fill with the all options
+
+if %N%==0 (goto INIT)
+endlocal
 goto DOWNLOADCENTER
-:: ----------------------------------------------------------
-:: -------------------DOWNLOAD CENTER END--------------------
-:: ----------------------------------------------------------
-:INIT
-exit
