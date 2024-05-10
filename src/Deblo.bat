9fec8580aa5e7b94a7e01989cbdaf1a5e4051292
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-05-11 00:00:48 +0200

    Implement memory usage #9

--- src/Deblo.bat
+++ src/Deblo.bat
@@ -105,43 +105,54 @@ echo %blue%      `-----'
 echo %white%   --------------------------------------------------------
 echo.
 
-echo    Loading, Please wait...  = [[1;31m 1/7 [m]
+echo    Loading, Please wait...  = [[1;31m 1/9 [m]
+setlocal
 
 :: Windows version
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-CimInstance Win32_OperatingSystem).Caption;"`) do Set version=%%a
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-CimInstance Win32_OperatingSystem).OSArchitecture;"`) do Set bits=%%a
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-CimInstance  Win32_OperatingSystem).Version;"`) do Set kernel=%%a
 
-echo    Loading, Please wait...  = [[1;31m 2/7 [m]
+echo    Loading, Please wait...  = [[1;31m 2/9 [m]
 
 ::CPU
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(((Get-CimInstance Win32_Processor).Name) -replace '\s+', ' ');"`) do Set cpu=%%a
 
-echo    Loading, Please wait...  = [[1;31m 3/7 [m]
+echo    Loading, Please wait...  = [[1;31m 3/9 [m]
 
 ::GPU
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-CimInstance Win32_DisplayConfiguration).DeviceName;"`) do Set gpu=%%a
 
-echo    Loading, Please wait...  = [[1;31m 4/7 [m]
+echo    Loading, Please wait...  = [[1;31m 4/9 [m]
+
+:: Fetch total used memory in GB
+for /f "usebackq delims=" %%a in (`%powershell% -Command "$os = Get-CimInstance Win32_OperatingSystem; [math]::Round(($os.TotalVisibleMemorySize - $os.FreePhysicalMemory) / 1MB, 2)"`) do Set "usedMemGb=%%a"
+
+echo    Loading, Please wait...  = [[1;31m 5/9 [m]
+
+:: Fetch total physical memory in GB
+for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum / 1gb"`) do Set "totalMemGb=%%a"
+
+echo    Loading, Please wait...  = [[1;31m 6/9 [m]
 
 ::Board
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-CimInstance Win32_BaseBoard | Select-Object Manufacturer, Product).Product;"`) do Set moboP=%%a
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-CimInstance Win32_BaseBoard | Select-Object Manufacturer, Product).Manufacturer;"`) do Set moboM=%%a
 
-echo    Loading, Please wait...  = [[1;31m 5/7 [m]
+echo    Loading, Please wait...  = [[1;31m 7/9 [m]
 
 ::Names
 for /f "usebackq delims=" %%a in (`%powershell% -Command "[System.Net.Dns]::GetHostName();"`) do Set userinfo=%%a
 for /f "usebackq delims=" %%a in (`%powershell% -Command "$env:USERNAME"`) do Set username=%%a
 
-echo    Loading, Please wait...  = [[1;31m 6/7 [m]
+echo    Loading, Please wait...  = [[1;31m 8/9 [m]
 
 ::Screen
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-WmiObject -Class Win32_VideoController).CurrentRefreshRate"`) do Set hz=%%a
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-WmiObject -Class Win32_VideoController).CurrentHorizontalResolution"`) do Set hozrs=%%a
 for /f "usebackq delims=" %%a in (`%powershell% -Command "(Get-WmiObject -Class Win32_VideoController).CurrentVerticalResolution"`) do Set verrs=%%a
 
-echo    Loading, Please wait...  = [[1;31m 7/7 [m]
+echo    Loading, Please wait...  = [[1;31m 9/9 [m]
 
 :: UpTime
 for /f %%a in ('WMIC OS GET lastbootuptime ^| find "."') DO set DTS=%%a
@@ -156,18 +167,21 @@ echo    Deblo.bat -[1;36m System information [m
 echo    Yet another (and not than ugly) Screenfetch. 
 echo    -----------------------------------------------
 echo  [m 
-echo  %logo1%      [1;34m%username%[m@[1;34m%userinfo%[m        
-echo  %logo1%      ---------------------
+echo  %logo1%      [1;34m%username%[m@[1;34m%userinfo%[m    
+echo  %logo1%      ---------------------    
 echo  %logo1%      [1;34mOS[m: %version% %bits%
 echo  %logo1%      [1;34mKernel[m: %kernel%
 echo  %logo1%      [1;34mCPU[m: %cpu% 
-echo [m                                   [1;34mMotherboard[m: %moboM% %moboP% 
-echo  %logo2%	   [1;34mGPU[m: %gpu%
-echo  %logo2%      [1;34mResolution[m: %hozrs% x %verrs% (%hz% Hz)
+echo  %logo1%      [1;34mMotherboard[m: %moboM% %moboP% 
+echo [m                                   [1;34mGPU[m: %gpu%
+echo  %logo2%	   [1;34mResolution[m: %hozrs% x %verrs% (%hz% Hz)
 echo  %logo2%      [1;34mBoot[m: %boot%
+echo  %logo2%      [1;34mMemory[m: %usedMemGb% / %totalMemGb% (GB)
+echo  %logo2%      
 echo  %logo2%      
 echo  %logo2%      Press any key for return to menu . . . 
 pause > nul
+endlocal
 goto INIT
 :: ----------------------------------------------------------
 :: --------------SYSTEM SHOW INFORMATION END-----------------
